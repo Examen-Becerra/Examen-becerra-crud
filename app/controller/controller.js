89609cd59deb09701736d558218958ee265f27b2
natalynohava <angienohava03@gmail.com>
2024-05-23 08:44:01 -0500

    insertar pago listo

--- app/controller/controller.js
+++ app/controller/controller.js
@@ -4,8 +4,6 @@ import { Success , Error } from "../message/msj.js"
 export const mostrarPago = async(req, res) => {
     const id = req.params['id'];
 
-    console.log(id);
-    console.log("hola");
 
     try {
         const respuesta = await db.query(`CALL SP_MOSTRAR('${id}')`)
@@ -21,6 +19,24 @@ export const listarPago = async(req, res) => {
         const respuesta = await db.query(`CALL SP_LISTAR()`)
 
         Success(req, res, 200, respuesta[0][0])
+        
+    } catch (error) {
+        Error(req, res, 400, error)
+    }
+}
+
+export const insertarPago  = async(req, res) => {
+    const {nombre, valor} = req.body
+
+    try {
+        const respuesta = await db.query(`CALL SP_CREAR('${nombre}', '${valor}')`);
+
+        if (respuesta[0]. affectedRows == 1) {
+            Success(req, res, 200, "El pago se ha insertado correctamente")
+        } else {
+            Error(req, res, 200, "El pago no se pudo insertar correctamente")
+            
+        }
         
     } catch (error) {
         Error(req, res, 400, error)
--- app/routes/routes_pago.js
+++ app/routes/routes_pago.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { listarPago, mostrarPago } from "../controller/controller.js";
+import { insertarPago, listarPago, mostrarPago } from "../controller/controller.js";
 
 const rutaPago = Router();
 
 rutaPago.get("/pagom/:id", mostrarPago);
 rutaPago.get("/pagol", listarPago);
+rutaPago.post("/pagoc", insertarPago);
 
 export default rutaPago;
